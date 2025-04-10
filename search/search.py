91d6b1f60e1da2f4de6437078dd9cc6c22a03ea8
pavander <pavander@umich.edu>
2025-04-10 15:53:16 -0400

    search function updates

--- search/search.py
+++ search/search.py
@@ -5,6 +5,7 @@ from itertools import combinations
 from postgres.helpers import get_db_conn
 
 def binary_and_search(terms):
+    """Returns reportids of all reports that list all drugs in terms."""
     conn = get_db_conn()
     curr = conn.cursor()
 
@@ -23,6 +24,7 @@ def binary_and_search(terms):
     return results
 
 def full_binary_search(terms):
+    """Performs a binary AND search for all combinations of drugs in terms."""
     queries = []
     # Loop through combination sizes from 2 to the size of the list
     for search_size in range(2, len(terms) + 1):
@@ -32,5 +34,31 @@ def full_binary_search(terms):
 
     for query in queries:
         results[query] = binary_and_search(query)
+
+    return results
+
+def get_med_info(drug):
+    """Collects medication information from the DB."""
+    conn = get_db_conn()
+    curr = conn.cursor()
+
+    # do something here
+
+    curr.close()
+    conn.close()
+
+    return
+
+def execute_query(drugs):
+    """Executes a query and returns results"""
+    results = {}
+
+    conn = get_db_conn()
+    curr = conn.cursor()
+
+    # do something here
+
+    curr.close()
+    conn.close()
 
     return results
