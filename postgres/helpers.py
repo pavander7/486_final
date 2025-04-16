b7360b12b438d1653d11cbeaedb9f033585dcbfc
pavander <pavander@umich.edu>
2025-04-16 17:33:43 -0400

    major problem with preprocessing!

--- postgres/helpers.py
+++ postgres/helpers.py
@@ -20,7 +20,7 @@ def rename_columns(df, prefix):
     df.rename(columns=new_columns, inplace=True)
     return df
 
-def convert_boolean (df, colnames, true_val = 2, false_val = 1):
+def convert_boolean (df, colnames, true_val = 1, false_val = 2):
     df = df.copy()
     for col in colnames:
         df[col] = df[col].apply(
